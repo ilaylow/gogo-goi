95bfff290c476a6e546dfe0c9d4f6068d72250bf
Chuen Ley Low <chuenleylow@gmail.com>
2025-07-12 02:05:19 +1000

    attempt new

--- database/create-daily.sql
+++ database/create-daily.sql
@@ -2,27 +2,58 @@
 DELETE FROM public.dailykanji WHERE 1=1;
 
 -- Insert in 5 words that the user got wrong recently (remove set of words they got right recently)
-INSERT INTO public.dailykanji (word)
-SELECT DISTINCT word FROM public.userinput
-WHERE iscorrect=false AND word NOT IN (
-    SELECT word FROM public.userinput
-    WHERE iscorrect = true
-    ORDER BY createtime DESC
-    LIMIT 100
-)
+SELECT word, furigana, meaning
+FROM (
+  SELECT DISTINCT ON (word)
+    k.word,
+    k.furigana,
+    k.meaning,
+    ui.createtime
+  FROM userinput ui
+  INNER JOIN kanji k ON ui.word = k.word
+  WHERE ui.iscorrect = false
+  AND word NOT IN (
+     SELECT word FROM public.userinput
+     WHERE iscorrect = true
+     ORDER BY createtime DESC
+     LIMIT 100
+  )
+  ORDER BY word, ui.createtime DESC
+) sub
 ORDER BY createtime DESC
+OFFSET 100
 LIMIT 5;
 
 -- Insert in 5 words that the user got right awhile ago
-INSERT INTO public.dailykanji (word)
-SELECT DISTINCT word from public.userinput
-WHERE iscorrect=true
+SELECT word, furigana, meaning
+FROM (
+  SELECT DISTINCT ON (word)
+    k.word,
+    k.furigana,
+    k.meaning,
+    ui.createtime
+  FROM userinput ui
+  INNER JOIN kanji k ON ui.word = k.word
+  WHERE ui.iscorrect = true
+  ORDER BY word, ui.createtime DESC
+) sub
 ORDER BY createtime DESC
-LIMIT 5 OFFSET 100;
+OFFSET 100
+LIMIT 5;
 
 -- Insert in 5 words that the user got wrong awhile ago
-INSERT INTO public.dailykanji (word)
-SELECT DISTINCT word from public.userinput
-WHERE iscorrect=false
+SELECT word, furigana, meaning
+FROM (
+  SELECT DISTINCT ON (word)
+    k.word,
+    k.furigana,
+    k.meaning,
+    ui.createtime
+  FROM userinput ui
+  INNER JOIN kanji k ON ui.word = k.word
+  WHERE ui.iscorrect = false
+  ORDER BY word, ui.createtime DESC
+) sub
 ORDER BY createtime DESC
-LIMIT 5 OFFSET 100;
+OFFSET 100
+LIMIT 5;
