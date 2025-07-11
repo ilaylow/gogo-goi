fc8f5eeb467514c6cb22a470ec3470fa206a43d0
Chuen Ley Low <chuenleylow@gmail.com>
2025-07-12 01:58:37 +1000

    fix sql

--- database/create-daily.sql
+++ database/create-daily.sql
@@ -3,7 +3,7 @@ DELETE FROM public.dailykanji WHERE 1=1;
 
 -- Insert in 5 words that the user got wrong recently (remove set of words they got right recently)
 INSERT INTO public.dailykanji (word)
-SELECT word FROM public.userinput
+SELECT DISTINCT word FROM public.userinput
 WHERE iscorrect=false AND word NOT IN (
     SELECT word FROM public.userinput
     WHERE iscorrect = true
@@ -15,14 +15,14 @@ LIMIT 5;
 
 -- Insert in 5 words that the user got right awhile ago
 INSERT INTO public.dailykanji (word)
-SELECT word from public.userinput
+SELECT DISTINCT word from public.userinput
 WHERE iscorrect=true
 ORDER BY createtime DESC
 LIMIT 5 OFFSET 100;
 
 -- Insert in 5 words that the user got wrong awhile ago
 INSERT INTO public.dailykanji (word)
-SELECT word from public.userinput
+SELECT DISTINCT word from public.userinput
 WHERE iscorrect=false
 ORDER BY createtime DESC
 LIMIT 5 OFFSET 100;
