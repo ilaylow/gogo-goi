a7721f6c02740c18bb4044ff83e9935c3e8123cd
Chuen Ley Low <chuenleylow@gmail.com>
2025-07-11 23:56:55 +1000

    create gha cron to execute sql against cloud cockroachdb instance

---
+++ database/create-daily.sql
@@ -0,0 +1,16 @@
+-- First clear the table
+DELETE FROM public.dailykanji WHERE 1=1;
+
+-- Insert in 5 words that the user got wrong recently
+INSERT INTO public.dailykanji (word)
+SELECT word FROM public.userinput
+WHERE iscorrect=false
+ORDER BY createtime DESC
+LIMIT 5;
+
+-- Insert in 5 words that the user got wrong awhile ago
+INSERT INTO public.dailykanji (word)
+SELECT word from public.userinput
+WHERE iscorrect=false
+ORDER BY createtime DESC
+LIMIT 5 OFFSET 100;
